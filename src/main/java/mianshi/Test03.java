6c421b96c5d552e03fe811e3f1be557458d152c1
xhw1990514 <1037901372@q.com>
2017-05-10 13:03:14 +0800

    初始化

---
+++ src/main/java/mianshi/Test03.java
@@ -0,0 +1,14 @@
+package mianshi;
+
+public class Test03 {
+	public static void main(String[] args){
+		//输出9*9口诀
+		for(int x=1;x<10;x++){
+			for(int y=1;y<=x;y++){
+				System.out.print(x+"*"+y+"="+x*y+"\t");
+			}
+			System.out.println();
+		}
+		
+	}
+}
---
+++ src/main/java/mianshi/Test04.java
@@ -0,0 +1,15 @@
+package mianshi;
+
+public class Test04 {
+	
+	public static void main(String[] args){
+		//&和&&区别
+		String str = null;
+		if(str!=null&&!"".equals(str)){
+			System.out.println("and ");
+		}
+		if(str!=null&!"".equals(str)){
+			System.out.println("or");
+		}
+	}
+}
