a52dc8d13b5292d77886575812a6195b01cb9b9a
xhw1990514 <1037901372@q.com>
2018-04-10 19:49:02 +0800

    1

--- src/main/java/note01/javase/TestString01.java
+++ src/main/java/note01/javase/TestString01.java
@@ -3,6 +3,18 @@ package note01.javase;
 public class TestString01 {
 	
 	public static void main(String[] args){
+		
+		//
+		String str1 = new String("hello");
+		String str2 = new String("hello");
+		String str3 = "hello";
+		String str4 = "hello";
+		
+		System.out.println(str3 == str1);
+		System.out.println(str1 == str2);
+	    System.out.println(str3 == str4);
+		
+	    
 		String str = new String("hello");
 		TestString01.modifyStr(str);
 		System.out.println(str);
